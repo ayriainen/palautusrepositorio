a4e3fa7ede768f4a6bb3513eaac5e82216c1bf37
ayriainen <lounastarjous@gmail.com>
2023-11-13 20:40:23 +0200

    Ei muutosta

---
+++ viikko2/project-reader/src/project.py
@@ -0,0 +1,22 @@
+class Project:
+    def __init__(self, name, description, dependencies, dev_dependencies, license, authors):
+        self.name = name
+        self.description = description
+        self.dependencies = dependencies
+        self.dev_dependencies = dev_dependencies
+        self.license = license
+        self.authors = authors
+
+    def _stringify_dependencies(self, dependencies):
+        return ", ".join(dependencies) if len(dependencies) > 0 else "-"
+
+    def __str__(self):
+        muunnettu_authors = "\n".join(self.authors) if self.authors else "-"
+        return (
+            f"Name: {self.name}"
+            f"\nDescription: {self.description or '-'}"
+            f"\nLicense: {self.license or '-'}"
+            f"\nAuthors:\n{muunnettu_authors}"
+            f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
+            f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
+        )
